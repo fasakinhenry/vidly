3650af5e287fa12bcf42fd64fdf1c3b4d8a0588d
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-03 18:01:40 +0100

    feat: create teh model route

---
+++ models/rentals.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const express = require('express');
+const router = express.Router();
+
+const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+      },
+      isGold: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+      },
+    }),
+    required: true,
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        minlength: 0,
+        trim: true,
+        maxlength: 255,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255,
+      },
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+  },
+});
+
+const Rental = mongoose.model('rental', rentalSchema);
+
+function validateMovie(movie) {
+  const schema = Joi.object({
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required(),
+  });
+  return schema.validate(movie);
+}
+
+exports.Rental = Rental;
+exports.validate = validateMovie;
