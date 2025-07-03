97b33131629a96069d2b80a4cdf8dc8968f2a173
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-03 17:59:01 +0100

    feat: create a pseudo route for rentals

--- routes/rentals.js
+++ routes/rentals.js
@@ -21,16 +21,56 @@ const rentalSchema = new mongoose.Schema({
         required: true,
         minlength: 5,
         maxlength: 50,
-      }
+      },
     }),
+    required: true,
   },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        minlength: 0,
+        trim: true,
+        maxlength: 255,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255,
+      },
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+  },
+});
+
+function validateMovie(movie) {
+  const schema = Joi.object({
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required(),
+  });
+  return schema.validate(movie);
+}
+
+// Get the list of rentals
+// GET /api/rentals
+router.get('/', async (req, res) => {
+  const rentals = await Rental.find().sort('-dateOut');
+  res.json(rentals);
 });
 
 // Create a new rental
 // POST /api/rentals
-router.get('/', async (req, res) => {
-  const rental = await Rental.find().select('name');
-});
-
-// Get the list of rentals
-// GET /api/rentals
