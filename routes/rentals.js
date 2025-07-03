002ea638bacadd89cb04c8350ce20bb145b6f986
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-03 18:03:54 +0100

    feat: implement getting all rentals

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,73 +1,17 @@
-const mongoose = require('mongoose');
-const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
-
-const rentalSchema = new mongoose.Schema({
-  customer: {
-    type: new mongoose.Schema({
-      name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50,
-      },
-      isGold: {
-        type: Boolean,
-        default: false,
-      },
-      phone: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50,
-      },
-    }),
-    required: true,
-  },
-  movie: {
-    type: new mongoose.Schema({
-      title: {
-        type: String,
-        required: true,
-        minlength: 0,
-        trim: true,
-        maxlength: 255,
-      },
-      dailyRentalRate: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 255,
-      },
-    }),
-    required: true,
-  },
-  dateOut: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
-  dateReturned: {
-    type: Date,
-  },
-  rentalFee: {
-    type: Number,
-    min: 0,
-  },
-});
-
-function validateMovie(movie) {
-  const schema = Joi.object({
-    customerId: Joi.string().required(),
-    movieId: Joi.string().required(),
-  });
-  return schema.validate(movie);
-}
+const { Rental, validate } = require('../models/rentals');
 
 // Get the list of rentals
 // GET /api/rentals
 router.get('/', async (req, res) => {
+  const rentals = await Rental.find().sort('-dateOut');
+  res.json(rentals);
+});
+
+// Get a particular rental item by the ID
+// GET /api/rental/:id
+router.get('/:id', async (req, res) => {
   const rentals = await Rental.find().sort('-dateOut');
   res.json(rentals);
 });
