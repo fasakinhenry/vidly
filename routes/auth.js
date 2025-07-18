5a45fbea30ea83b8707e39f4394e19aa885e2102
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-18 17:57:47 +0100

    feat: add the auth route

---
+++ routes/auth.js
@@ -0,0 +1,23 @@
+const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
+const { User, validate } = require('../models/users.js');
+const bcrypt = require('bcrypt');
+
+// Create a new genre
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send('User already registered');
+
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  await user.save();
+  res.status(201).json(_.pick(user, ['_id','name', 'email']));
+});
+
+module.exports = router;
