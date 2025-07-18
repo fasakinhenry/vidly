27b051a2dcd34df15fa15f4211e0a797bb1500ee
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-18 17:56:57 +0100

    feat: add code for hashing passwords

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 const { User, validate } = require('../models/users.js');
+const bcrypt = require('bcrypt');
 
 // Create a new genre
 router.post('/', async (req, res) => {
@@ -12,6 +13,8 @@ router.post('/', async (req, res) => {
   if (user) return res.status(400).send('User already registered');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
   res.status(201).json(_.pick(user, ['_id','name', 'email']));
